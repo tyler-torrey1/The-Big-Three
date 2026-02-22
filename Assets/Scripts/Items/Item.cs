8b9996683d04584b52e7a723382d769ba499a33e
Tyler <tylertorrey16@gmail.com>
2026-02-21 22:26:55 -0500

    Does a bunch of stuff

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -7,20 +7,39 @@ public class Item : MonoBehaviour {
 
     private Inventory _inventory;
 
+    private StageManager _stageManager;
+
+    private int _maxStageIndex = 4;
+
     public void Awake() {
         this._coll2D = this.GetComponent<BoxCollider2D>();
         this._inventory = GlobalManager.player.GetComponent<Inventory>();
-        Debug.Log("Inventory initiated as:" + this._inventory);
+        this._stageManager = this.GetComponentInParent<StageManager>();
+    }
+    public void Start() {
+        this._stageManager.OnStageChanged += this.SetCurrentStage;
+        this.SetCurrentStage(0);
     }
     public void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Player") {
             bool itemPickedUp = this._inventory.PickupItem(this);
             if (itemPickedUp) {
-                Destroy(gameObject);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+    public void SetCurrentStage(int newStageIndex) {
+        if (newStageIndex == this._maxStageIndex) {
+            this.gameObject.SetActive(true);
+        } else {
+            if (this.gameObject.activeInHierarchy) {
+                this.gameObject.SetActive(false);
             }
         }
     }
 }
+
+
 
 public enum ItemType {
     Key,
